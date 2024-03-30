980f9ce5023be4994053ca966e42d7981b64d949
manaralrr <160352954+manaralrr@users.noreply.github.com>
2024-03-30 21:54:39 +0530

    Add files via upload

---
+++ exhibition.py
@@ -0,0 +1,6 @@
+class Exhibition:
+    def __init__(self, name, location, start_date, end_date):
+        self.name = name
+        self.location = location
+        self.start_date = start_date
+        self.end_date = end_date
