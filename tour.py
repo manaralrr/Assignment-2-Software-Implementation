90078a2fe4fb9766ebcf1eead8a317ecf042a591
manaralrr <160352954+manaralrr@users.noreply.github.com>
2024-03-30 21:58:12 +0530

    Add files via upload

---
+++ tour.py
@@ -0,0 +1,6 @@
+class Tour:
+    def __init__(self, date, guide, visitor_group, exhibition):
+        self.date = date
+        self.guide = guide
+        self.visitor_group = visitor_group
+        self.exhibition = exhibition
