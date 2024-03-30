534e27bee0416da29428a57689ba171f5ec59507
manaralrr <160352954+manaralrr@users.noreply.github.com>
2024-03-30 22:29:04 +0530

    Update event.py

--- event.py
+++ event.py
@@ -1,7 +1,14 @@
 class Event:
+
+    events = []
+    
     def __init__(self, name, location, start_date, end_date, ticket_price):
         self.name = name
         self.location = location
         self.start_date = start_date
         self.end_date = end_date
         self.ticket_price = ticket_price
+        Event.events.append(self)
+
+    def get_all_events():
+        return Event.events
