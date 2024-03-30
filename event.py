4465240b892e9001c7469ae68685334579b014a8
manaralrr <160352954+manaralrr@users.noreply.github.com>
2024-03-30 21:57:07 +0530

    Add files via upload

---
+++ event.py
@@ -0,0 +1,7 @@
+class Event:
+    def __init__(self, name, location, start_date, end_date, ticket_price):
+        self.name = name
+        self.location = location
+        self.start_date = start_date
+        self.end_date = end_date
+        self.ticket_price = ticket_price
