70a4ee37b9bafd625539b790d8429ee03e9da38e
manaralrr <160352954+manaralrr@users.noreply.github.com>
2024-03-30 22:19:13 +0530

    Update artwork.py

--- artwork.py
+++ artwork.py
@@ -6,9 +6,15 @@ class Location(Enum):
     OUTDOOR_SPACE = 3
 
 class Artwork:
+
+    artworks = []
+    
     def __init__(self, title, artist, date_of_creation, historical_significance, location):
         self.title = title
         self.artist = artist
         self.date_of_creation = date_of_creation
         self.historical_significance = historical_significance
         self.location = location
+
+    def get_all_artworks():
+        return Artwork.artworks
