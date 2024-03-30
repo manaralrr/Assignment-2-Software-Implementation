1a8d25c7545bbe2986d7957a077c960d5da55c30
manaralrr <160352954+manaralrr@users.noreply.github.com>
2024-03-30 22:30:10 +0530

    Update exhibition.py

--- exhibition.py
+++ exhibition.py
@@ -1,6 +1,13 @@
 class Exhibition:
+
+    exhibitions = []
+    
     def __init__(self, name, location, start_date, end_date):
         self.name = name
         self.location = location
         self.start_date = start_date
         self.end_date = end_date
+        Exhibition.exhibitions.append(self)
+
+    def get_all_exhibitions():
+        return Exhibition.exhibitions
